e0cabe1120d106b24acc89df5873b69c32ca83f3
PJK <me@pavelkalvoda.com>
2017-06-22 11:53:15 +0200

    TF 1.2 updates

--- subpixel.py
+++ subpixel.py
@@ -8,9 +8,9 @@ def _phase_shift(I, r):
     bsize = tf.shape(I)[0] # Handling Dimension(None) type for undefined batch dim
     X = tf.reshape(I, (bsize, a, b, r, r))
     X = tf.transpose(X, (0, 1, 2, 4, 3))  # bsize, a, b, 1, 1
-    X = tf.split(1, a, X)  # a, [bsize, b, r, r]
+    X = tf.split(X, a, 1)  # a, [bsize, b, r, r]
     X = tf.concat(2, [tf.squeeze(x, axis=1) for x in X])  # bsize, b, a*r, r
-    X = tf.split(1, b, X)  # b, [bsize, a*r, r]
+    X = tf.split(X, b, 1)  # b, [bsize, a*r, r]
     X = tf.concat(2, [tf.squeeze(x, axis=1) for x in X])  # bsize, a*r, b*r
     return tf.reshape(X, (bsize, a*r, b*r, 1))
 
